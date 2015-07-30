f3d1162bbc6230730554a372c6b9db687b9dc038
hx78 <hangxing78@163.com>
2015-07-30 10:11:20 +0800

    modify sizeof and add .gitignore

--- funTest/sizeof/sizeof.c
+++ funTest/sizeof/sizeof.c
@@ -25,15 +25,30 @@ union union1
 enum enum1{c,i,d,f,l};
 
 
-void ca_len(char *str)
+void ca_point(char *str)
 {
 	int len = 0;
 	len = sizeof(str);
-	DEBUG("len1 = %d",len);
+	DEBUG("sizeof str = %d",len);
 
 	len = strlen(str);
-	DEBUG("len2 = %d",len);
+	DEBUG("strlen str = %d",len);
 }
+
+void ca_arrary(char a[])
+{
+	DEBUG("ca_arrary a = %s",a);
+
+	int len = 0;
+	len = sizeof(a);
+	DEBUG("sizeof a = %d",len);
+
+	
+	len = strlen(a);
+	DEBUG("strlen a = %d",len);
+}
+
+
 
 int main()
 {
@@ -55,12 +70,19 @@ int main()
 
 	DEBUG("sizeof enum=%ld",sizeof(enum enum1));
 
-	DEBUG("\n\nstruct struct1=%ld",sizeof(struct struct1));
+	
+	//C/C++中不能传数组,只能传指针,所以任何数组都会隐式转成指针形式进行操作,所以"类型"还是指针
+	DEBUG("sizeof struct struct1=%ld",sizeof(struct struct1));
 	struct struct1 *ptr_struct1 = (struct struct1 *) malloc( sizeof(struct struct1) );
 	sprintf(ptr_struct1->s,"%s","12345");
-	ca_len(ptr_struct1->s);
+	ca_point(ptr_struct1->s);
 	free(ptr_struct1);	
 
+	char a[10] = "12345";
+	DEBUG("a = %s",a);
+	DEBUG("sizeof char[10]=%ld",sizeof(a));
+	ca_arrary(a);
+	
 	return 0;
 
 }
